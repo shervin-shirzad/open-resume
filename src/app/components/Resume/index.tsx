b8e6b379aecf9e21c370a49abeb08526254497f2
Shervin Shirzad <50713047+shervin-shirzad@users.noreply.github.com>
2025-10-11 19:14:14 +0400

    Update index.tsx

--- src/app/components/Resume/index.tsx
+++ src/app/components/Resume/index.tsx
@@ -1,12 +1,4 @@
 "use client";
-import MyModernTemplate from "./templates/MyModernTemplate";
-
-const templates = {
-  default: DefaultTemplate,
-  modern: MyModernTemplate
-};
-
-
 import { useState, useMemo } from "react";
 import { ResumeIframeCSR } from "components/Resume/ResumeIFrame";
 import { ResumePDF } from "components/Resume/ResumePDF";
